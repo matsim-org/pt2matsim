39b45830ca083cde15954085efc41ce0aa072bff
polettif <flavio.poletti@hotmail.ch>
2017-02-21 11:11:02 +0100

    GtfsFeedImpl test

--- src/test/java/org/matsim/pt2matsim/gtfs/GtfsConverterTest.java
+++ src/test/java/org/matsim/pt2matsim/gtfs/GtfsConverterTest.java
@@ -11,20 +11,21 @@ import org.matsim.pt.transitSchedule.api.TransitSchedule;
  */
 public class GtfsConverterTest {
 
+	private GtfsFeed gtfsFeed;
 	private GtfsConverter gtfsConverter;
 
 	@Before
 	public void convert() throws Exception {
 		String coordinateSystem = "EPSG:2032";
 
-		GtfsFeed gtfsFeed = new GtfsFeedImpl("test/analysis/addisoncounty-vt-us-gtfs/");
+		gtfsFeed = new GtfsFeedImpl("test/analysis/addisoncounty-vt-us-gtfs/");
 
 		gtfsConverter = new GtfsConverter(gtfsFeed);
 		gtfsConverter.convert(GtfsConverter.ALL_SERVICE_IDS, coordinateSystem);
 	}
 
 	@Test
-	public void numberOfTransitRoutes() {
+	public void numberOfRoutes() {
 		TransitSchedule schedule = gtfsConverter.getSchedule();
 
 		int nTransitLines = 0;
@@ -35,7 +36,7 @@ public class GtfsConverterTest {
 			nTransitRoutes += tl.getRoutes().size();
 		}
 
-		Assert.assertEquals(11, nTransitLines);
+		Assert.assertEquals(gtfsFeed.getRoutes().size(), nTransitLines);
 		Assert.assertEquals(75, nTransitRoutes);
 	}
 
