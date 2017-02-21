a51202fbbee654212b72180eaab50e1b64b49d7a
polettif <flavio.poletti@hotmail.ch>
2017-02-21 11:11:02 +0100

    GtfsFeedImpl test

---
+++ src/test/java/org/matsim/pt2matsim/gtfs/GtfsFeedImplTest.java
@@ -0,0 +1,28 @@
+package org.matsim.pt2matsim.gtfs;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author polettif
+ */
+public class GtfsFeedImplTest {
+
+	private GtfsFeed feed;
+
+	@Before
+	public void prepare() {
+		feed = new GtfsFeedImpl("test/analysis/addisoncounty-vt-us-gtfs/");
+	}
+
+	@Test
+	public void statistics() throws Exception {
+		Assert.assertEquals(114, feed.getStops().size());
+		Assert.assertEquals(11, feed.getRoutes().size());
+		Assert.assertEquals(15, feed.getServices().size());
+		Assert.assertEquals(50, feed.getShapes().size());
+		Assert.assertFalse(feed.usesFrequencies());
+	}
+
+}
