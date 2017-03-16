2951079634a992432b7e818922d67a0d55553b79
polettif <flavio.poletti@hotmail.ch>
2017-03-16 09:16:40 +0100

    refactored test coords

--- src/test/java/org/matsim/pt2matsim/tools/ShapeToolsTest.java
+++ src/test/java/org/matsim/pt2matsim/tools/ShapeToolsTest.java
@@ -14,6 +14,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.matsim.pt2matsim.tools.CoordToolsTest.*;
+
 /**
  * @author polettif
  */
@@ -29,33 +31,33 @@ public class ShapeToolsTest {
 		Map<Id<RouteShape>, RouteShape> shapes = new HashMap<>();
 
 		RouteShape shapeA1 = new GtfsShape("A1");
-		shapeA1.addPoint(NetworkToolsTest.coordE, 1);
-		shapeA1.addPoint(NetworkToolsTest.coordD, 2);
-		shapeA1.addPoint(NetworkToolsTest.coordA, 3);
-		shapeA1.addPoint(NetworkToolsTest.coordX, 4);
-		shapeA1.addPoint(NetworkToolsTest.coordB, 5);
-		shapeA1.addPoint(NetworkToolsTest.coordI, 6);
+		shapeA1.addPoint(coordE, 1);
+		shapeA1.addPoint(coordD, 2);
+		shapeA1.addPoint(coordA, 3);
+		shapeA1.addPoint(coordX, 4);
+		shapeA1.addPoint(coordB, 5);
+		shapeA1.addPoint(coordI, 6);
 		shapes.put(shapeA1.getId(), shapeA1);
 
 		RouteShape shapeA2 = new GtfsShape("A2");
-		shapeA2.addPoint(offset(NetworkToolsTest.coordE), 6);
-		shapeA2.addPoint(offset(NetworkToolsTest.coordD), 5);
-		shapeA2.addPoint(offset(NetworkToolsTest.coordA), 4);
-		shapeA2.addPoint(offset(NetworkToolsTest.coordX), 3);
-		shapeA2.addPoint(offset(NetworkToolsTest.coordB), 2);
-		shapeA2.addPoint(offset(NetworkToolsTest.coordI), 1);
+		shapeA2.addPoint(offset(coordE), 6);
+		shapeA2.addPoint(offset(coordD), 5);
+		shapeA2.addPoint(offset(coordA), 4);
+		shapeA2.addPoint(offset(coordX), 3);
+		shapeA2.addPoint(offset(coordB), 2);
+		shapeA2.addPoint(offset(coordI), 1);
 		shapes.put(shapeA2.getId(), shapeA2);
 
 		RouteShape shapeB = new GtfsShape("B");
-		shapeB.addPoint(NetworkToolsTest.coordE, 1);
-		shapeB.addPoint(NetworkToolsTest.coordW, 2);
-		shapeB.addPoint(NetworkToolsTest.coordD, 3);
-		shapeB.addPoint(NetworkToolsTest.coordC, 4);
-		shapeB.addPoint(NetworkToolsTest.coordX, 5);
-		shapeB.addPoint(NetworkToolsTest.coordA, 6);
-		shapeB.addPoint(NetworkToolsTest.coordH, 7);
-		shapeB.addPoint(NetworkToolsTest.coordZ, 8);
-		shapeB.addPoint(NetworkToolsTest.coordI, 9);
+		shapeB.addPoint(coordE, 1);
+		shapeB.addPoint(coordW, 2);
+		shapeB.addPoint(coordD, 3);
+		shapeB.addPoint(coordC, 4);
+		shapeB.addPoint(coordX, 5);
+		shapeB.addPoint(coordA, 6);
+		shapeB.addPoint(coordH, 7);
+		shapeB.addPoint(coordZ, 8);
+		shapeB.addPoint(coordI, 9);
 		shapes.put(shapeB.getId(), shapeB);
 
 		return shapes;
@@ -75,10 +77,10 @@ public class ShapeToolsTest {
 
 	@Test
 	public void calcMinDistanceToShape() throws Exception {
-		Assert.assertEquals(0, ShapeTools.calcMinDistanceToShape(NetworkToolsTest.coordX, shapeB), d);
+		Assert.assertEquals(0, ShapeTools.calcMinDistanceToShape(coordX, shapeB), d);
 		Assert.assertEquals(5, ShapeTools.calcMinDistanceToShape(new Coord(-5, -5), shapeB), d);
 
-		Coord bx = CoordTools.calcNewPoint(NetworkToolsTest.coordX, CoordTools.getAzimuth(NetworkToolsTest.coordX, NetworkToolsTest.coordB), CoordUtils.calcEuclideanDistance(NetworkToolsTest.coordX, NetworkToolsTest.coordB) / 2);
+		Coord bx = CoordTools.calcNewPoint(coordX, CoordTools.getAzimuth(coordX, coordB), CoordUtils.calcEuclideanDistance(coordX, coordB) / 2);
 		Assert.assertEquals(0, ShapeTools.calcMinDistanceToShape(bx, shapeA1), d);
 	}
 
