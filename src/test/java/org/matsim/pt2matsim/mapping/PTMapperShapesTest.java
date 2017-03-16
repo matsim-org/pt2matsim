209727946a25e484e1dab74a9c4f98b29ada24b5
polettif <flavio.poletti@hotmail.ch>
2017-03-16 10:28:08 +0100

    added shapes mapper test

---
+++ src/test/java/org/matsim/pt2matsim/mapping/PTMapperShapesTest.java
@@ -0,0 +1,74 @@
+package org.matsim.pt2matsim.mapping;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.pt.transitSchedule.api.TransitLine;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
+import org.matsim.pt.transitSchedule.api.TransitSchedule;
+import org.matsim.pt2matsim.config.PublicTransitMappingConfigGroup;
+import org.matsim.pt2matsim.config.PublicTransitMappingStrings;
+import org.matsim.pt2matsim.lib.RouteShape;
+import org.matsim.pt2matsim.mapping.networkRouter.ScheduleRouters;
+import org.matsim.pt2matsim.mapping.networkRouter.ScheduleRoutersWithShapes;
+import org.matsim.pt2matsim.tools.NetworkToolsTest;
+import org.matsim.pt2matsim.tools.ScheduleTools;
+import org.matsim.pt2matsim.tools.ScheduleToolsTest;
+import org.matsim.pt2matsim.tools.ShapeToolsTest;
+import org.matsim.pt2matsim.tools.debug.ScheduleCleaner;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.matsim.pt2matsim.mapping.PTMapperTest.initPTMConfig;
+
+/**
+ * @author polettif
+ */
+public class PTMapperShapesTest {
+	public Network network;
+	public TransitSchedule schedule;
+	public PublicTransitMappingConfigGroup ptmConfig;
+
+	@Before
+	public void prepare() {
+		ptmConfig = initPTMConfig();
+		network = NetworkToolsTest.initNetwork();
+		schedule = ScheduleToolsTest.initSchedule();
+		ScheduleCleaner.combineChildStopsToParentStop(schedule);
+		ScheduleCleaner.removeMapping(schedule);
+		ScheduleCleaner.removeNotUsedStopFacilities(schedule);
+
+		Map<Id<RouteShape>, RouteShape> shapes = ShapeToolsTest.initShapes();
+		ScheduleRouters scheduleRouters = new ScheduleRoutersWithShapes(ptmConfig, schedule, network, shapes, 10.0, 99);
+
+		new PTMapper(ptmConfig, schedule, network, scheduleRouters).run();
+
+		ScheduleCleaner.removeNotUsedStopFacilities(schedule);
+	}
+
+	@Test
+	public void allowedModes() {
+		for(Link l : network.getLinks().values()) {
+			Assert.assertFalse(l.getAllowedModes().contains(PublicTransitMappingStrings.ARTIFICIAL_LINK_MODE));
+		}
+	}
+
+	@Test
+	public void linkSequences() {
+		TransitSchedule initSchedule = ScheduleToolsTest.initSchedule();
+
+		for(TransitLine l : schedule.getTransitLines().values()) {
+			for(TransitRoute r : l.getRoutes().values()) {
+				TransitRoute initRoute = initSchedule.getTransitLines().get(l.getId()).getRoutes().get(r.getId());
+				List<Id<Link>> initLinkIds = ScheduleTools.getTransitRouteLinkIds(initRoute);
+				List<Id<Link>> linkIds = ScheduleTools.getTransitRouteLinkIds(r);
+				Assert.assertEquals(initLinkIds, linkIds);
+			}
+		}
+	}
+
+}
