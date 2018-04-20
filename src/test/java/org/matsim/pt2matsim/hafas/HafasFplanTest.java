11d7747b3b61cc66adcbbc6f53453c2c7106d875
Johannes Lieberherr <johannes.lieberherr@ttools.ch>
2018-04-20 15:26:30 +0200

    cleanup

--- src/test/java/org/matsim/pt2matsim/hafas/HafasFplanTest.java
+++ src/test/java/org/matsim/pt2matsim/hafas/HafasFplanTest.java
@@ -2,8 +2,6 @@ package org.matsim.pt2matsim.hafas;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.matsim.core.utils.geometry.CoordinateTransformation;
-import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.pt.transitSchedule.api.TransitSchedule;
 import org.matsim.pt2matsim.tools.ScheduleTools;
 import org.matsim.vehicles.VehicleUtils;
@@ -16,13 +14,11 @@ public class HafasFplanTest {
     @Test
     public void simpleFplanTest() throws IOException {
 
+        String hafasFolder = "test/FPLAN_HAFAS/";
+
         TransitSchedule schedule = ScheduleTools.createSchedule();
         Vehicles vehicles = VehicleUtils.createVehiclesContainer();
-        String hafasFolder = "test/FPLAN_HAFAS/";
-        String cs = "EPSG:2056";
-        CoordinateTransformation ct = TransformationFactory.getCoordinateTransformation("WGS84", cs);
-
-        HafasConverter.run(hafasFolder, schedule, ct, vehicles);
+        HafasConverter.run(hafasFolder, schedule, null, vehicles);
 
         int nbRoutes = schedule.getTransitLines().values().stream().flatMap(l -> l.getRoutes().values().stream()).collect(Collectors.toList()).size();
         Assert.assertEquals(2, nbRoutes);
