7ef0740119868b6de217ecbbf1130e37295a0811
Flavio Poletti <flavio.poletti@hotmail.ch>
2020-10-23 15:53:58 +0200

    add example workflow class

---
+++ src/main/java/org/matsim/pt2matsim/run/ExampleWorkflow.java
@@ -0,0 +1,46 @@
+package org.matsim.pt2matsim.run;
+
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.pt.transitSchedule.api.TransitSchedule;
+import org.matsim.pt2matsim.config.OsmConverterConfigGroup;
+import org.matsim.pt2matsim.config.PublicTransitMappingConfigGroup;
+import org.matsim.pt2matsim.mapping.PTMapper;
+import org.matsim.pt2matsim.run.gis.Network2Geojson;
+import org.matsim.pt2matsim.run.gis.Schedule2Geojson;
+import org.matsim.pt2matsim.tools.NetworkTools;
+import org.matsim.pt2matsim.tools.ScheduleTools;
+
+/**
+ * This is an example workflow purely. The network and schedule files are placeholders and
+ * not part of the GitHub repository
+ *
+ * @author polettif
+ */
+public class ExampleWorkflow {
+
+	public static void main(String[] args) {
+		// Convert Network
+		OsmConverterConfigGroup osmConfig = OsmConverterConfigGroup.loadConfig("osm2matsimConfig.xml");
+		Osm2MultimodalNetwork.run(osmConfig); // or just: Osm2MultimodalNetwork.run("osm2matsimConfig.xml");
+
+		// convert schedule
+		String unmappedSchedule = "intermediate/schedule_unmapped.xml.gz";
+		Gtfs2TransitSchedule.run("gtfs", "dayWithMostTrips", osmConfig.getOutputCoordinateSystem(), unmappedSchedule, "output/vehicles_unmapped.xml.gz");
+
+		// setup public transit mapper
+		PublicTransitMappingConfigGroup mapperConfig = PublicTransitMappingConfigGroup.createDefaultConfig();
+		Network network = NetworkTools.readNetwork(osmConfig.getOutputNetworkFile());
+		TransitSchedule schedule = ScheduleTools.readTransitSchedule(unmappedSchedule);
+
+		// map schedule to network
+		PTMapper.mapScheduleToNetwork(schedule,  network, mapperConfig);
+
+		// write results
+		NetworkTools.writeNetwork(network, "output/network.xml.gz");
+		ScheduleTools.writeTransitSchedule(schedule, "output/schedule.xml.gz");
+
+		// Write geojson result
+		Network2Geojson.run(osmConfig.getOutputCoordinateSystem(), network, "output/network.geojson");
+		Schedule2Geojson.run(osmConfig.getOutputCoordinateSystem(), schedule, "output/schedule.geojson");
+	}
+}
