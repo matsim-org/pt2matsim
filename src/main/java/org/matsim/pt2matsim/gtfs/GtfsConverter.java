c319185c972b94c867f9bffcd7d0387fcaf82296
Steffen Axer <26229392+steffenaxer@users.noreply.github.com>
2020-11-13 14:23:55 +0100

    Update GtfsConverter.java
    Fixed number of required stopTimes

--- src/main/java/org/matsim/pt2matsim/gtfs/GtfsConverter.java
+++ src/main/java/org/matsim/pt2matsim/gtfs/GtfsConverter.java
@@ -223,7 +223,7 @@ public class GtfsConverter {
 	protected TransitRoute createTransitRoute(Trip trip, Map<Id<TransitStopFacility>, TransitStopFacility> stopFacilities) {
 		Id<RouteShape> shapeId = trip.getShape() != null ? trip.getShape().getId() : null;
 
-		if(trip.getStopTimes().size() >= 2) {
+		if(trip.getStopTimes().size() <= 1) {
 			noStopTimeTrips++;
 			return null;
 		}
