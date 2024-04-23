f056251f641b4c372a049e5befcc3622ba7ad4a0
marecabo <23156476+marecabo@users.noreply.github.com>
2024-04-23 13:47:53 +0200

    Add comment that turn restrictions are not tested but kept enabled in OsmMultimodalNetworkConverterTest

--- src/test/java/org/matsim/pt2matsim/osm/OsmMultimodalNetworkConverterTest.java
+++ src/test/java/org/matsim/pt2matsim/osm/OsmMultimodalNetworkConverterTest.java
@@ -34,7 +34,7 @@ public class OsmMultimodalNetworkConverterTest {
 		osmConfig.setOsmFile("test/osm/GerasdorfArtificialLanesAndMaxspeed.osm");
 		osmConfig.setOutputNetworkFile("test/osm/GerasdorfArtificialLanesAndMaxspeed.xml.gz");
 		osmConfig.setMaxLinkLength(1000);
-		osmConfig.parseTurnRestrictions = true;
+		osmConfig.parseTurnRestrictions = true; // turn restrictions not explicitly tested in this class
 
 		// read OSM file
 		OsmData osm = new OsmDataImpl();
