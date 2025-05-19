9ae74d6e8c7d8416a706d01f9b06a29b75bb7830
Hannes <23156476+marecabo@users.noreply.github.com>
2025-05-19 10:14:13 +0200

    Fix network cleaning with turn restrictions (#289)
    * Bump matsim from 2024.0 to 2025.0-2025w07
    
    * Set Java to 21
    
    * Set source encoding to UTF-8
    
    * Clean network from invalid DisallowedNextLinks (#263)
    
    * Bump matsim from 2025.0-2025w07 to 2025.0-2025w08
    
    * Clean network from invalid DisallowedNextLinks
    
    * Adjust test vectors
    
    * Revert "Set Java to 21"
    
    This reverts commit 2bcd989e0eb26395f066411a2991939b4d55a3d2.
    
    * Revert "Set source encoding to UTF-8"
    
    This reverts commit 35f707239878ed8828c2efe17e6d43acf60873dc.
    
    * Bump Java from 17 to 21
    
    * Bump matsim from 2025.0-2025w08 to 2025.0
    
    * Switch RoutableSubnetworkParams to new parameter notation
    
    * Fix copying of turn restriction attributes
    
    * Bump matsim from 2025.0 to 2026.0-PR3933
    
    * Bump matsim from 2025.0 to 2026.0-2025w20
    
    Bump matsim from 2025.0-2025w20 to 2025.0-PR3945
    
    * Consider pt when parsing turn restrictions and all pt modes are excluded
    
    * Add trolleybus to OSM2MATSim modes
    
    * Clean converted subnetworks with turn restrictions
    
    * Bump matsim from 2026.0-PR3945 to 2026.0-PR3956
    
    * Remove debug output

--- src/test/java/org/matsim/pt2matsim/osm/RoutableSubnetworkTest.java
+++ src/test/java/org/matsim/pt2matsim/osm/RoutableSubnetworkTest.java
@@ -2,15 +2,16 @@ package org.matsim.pt2matsim.osm;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.core.config.ConfigGroup;
+import org.matsim.core.network.NetworkUtils;
 import org.matsim.pt2matsim.config.OsmConverterConfigGroup;
 import org.matsim.pt2matsim.osm.lib.OsmData;
 import org.matsim.pt2matsim.osm.lib.OsmDataImpl;
 import org.matsim.pt2matsim.osm.lib.OsmFileReader;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -24,14 +25,14 @@ class RoutableSubnetworkTest {
 		// setup config
 		OsmConverterConfigGroup osmConfig = OsmConverterConfigGroup.createDefaultConfig();
 		osmConfig.setOutputCoordinateSystem("WGS84");
-		osmConfig.setOsmFile("test/osm/WaterlooCityCentre.osm");
-		osmConfig.setOutputNetworkFile("test/output/WaterlooCityCentre.xml.gz");
+		osmConfig.setOsmFile("test/osm/routable_subnetwork.osm");
+		osmConfig.setOutputNetworkFile("test/output/routable_subnetwork.xml");
 		osmConfig.setMaxLinkLength(20);
-		
-		// Add car_passenger to all car links		
+
+		// Add car_passenger to all car links
 		for (ConfigGroup params : osmConfig.getParameterSets(OsmConverterConfigGroup.OsmWayParams.SET_NAME)) {
 			OsmConverterConfigGroup.OsmWayParams wayParams = (OsmConverterConfigGroup.OsmWayParams) params;
-			
+
 			if (wayParams.getAllowedTransportModes().contains("car")) {
 				Set<String> allowedTransportModes = new HashSet<>(wayParams.getAllowedTransportModes());
 				allowedTransportModes.add("car_passenger");
@@ -46,22 +47,91 @@ class RoutableSubnetworkTest {
 		// I) Convert without a network layer for car_passenger
 		OsmMultimodalNetworkConverter converter = new OsmMultimodalNetworkConverter(osm);
 		converter.convert(osmConfig);
-		
+
+		Network network1 = converter.getNetwork();
+		// NetworkUtils.writeNetwork(network1, "1.xml");
+
+		// Since some car links are not connected, we expect that there are more (uncleaned) passenger links now
+		Link l2 = network1.getLinks().get(Id.createLinkId("2"));
+		Assertions.assertEquals(Set.of("car_passenger"), l2.getAllowedModes());
+		int carLinks = countModeLinks(network1, "car");
+		int carPassengerLinks = countModeLinks(network1, "car_passenger");
+		Assertions.assertEquals(carLinks + 1, carPassengerLinks);
+
+		// II) Convert with a network layer for car_passenger
+		osmConfig.addParameterSet(new OsmConverterConfigGroup.RoutableSubnetworkParams("car_passenger", Set.of("car")));
+
+		OsmMultimodalNetworkConverter converter2 = new OsmMultimodalNetworkConverter(osm);
+		converter2.convert(osmConfig);
+		Network network2 = converter2.getNetwork();
+		// NetworkUtils.writeNetwork(network2, "2.xml");
+
+		NetworkUtils.cleanNetwork(network2, Set.of("car"));
+		// NetworkUtils.writeNetwork(network2, "3.xml");
+
+		int carLinks2 = countModeLinks(network2, "car");
+		int carPassengerLinks2 = countModeLinks(network2, "car_passenger");
+
+		System.out.println("carLinks: " + carLinks);
+		System.out.println("carPassengerLinks: " + carPassengerLinks);
+		System.out.println("carLinks2: " + carLinks2);
+		System.out.println("carPassengerLinks2: " + carPassengerLinks2);
+
+		// Now car_passenger should be cleaned just as car... Thus it should be the same number.
+		Assertions.assertEquals(carLinks2, carPassengerLinks2);
+	}
+
+	@Test
+	void customSubnetworksWaterloo() {
+		// setup config
+		OsmConverterConfigGroup osmConfig = OsmConverterConfigGroup.createDefaultConfig();
+		osmConfig.setOutputCoordinateSystem("WGS84");
+		osmConfig.setOsmFile("test/osm/WaterlooCityCentre.osm");
+		osmConfig.setOutputNetworkFile("test/output/WaterlooCityCentre.xml.gz");
+		osmConfig.setMaxLinkLength(20);
+
+		// Add car_passenger to all car links
+		for (ConfigGroup params : osmConfig.getParameterSets(OsmConverterConfigGroup.OsmWayParams.SET_NAME)) {
+			OsmConverterConfigGroup.OsmWayParams wayParams = (OsmConverterConfigGroup.OsmWayParams) params;
+
+			if (wayParams.getAllowedTransportModes().contains("car")) {
+				Set<String> allowedTransportModes = new HashSet<>(wayParams.getAllowedTransportModes());
+				allowedTransportModes.add("car_passenger");
+				wayParams.setAllowedTransportModes(allowedTransportModes);
+			}
+		}
+
+		// read OSM file
+		OsmData osm = new OsmDataImpl();
+		new OsmFileReader(osm).readFile(osmConfig.getOsmFile());
+
+		// I) Convert without a network layer for car_passenger
+		OsmMultimodalNetworkConverter converter = new OsmMultimodalNetworkConverter(osm);
+		converter.convert(osmConfig);
+
+		// Since some car links are not connected, we expect that there are more (uncleaned) passenger links now
 		int carLinks = countModeLinks(converter.getNetwork(), "car");
 		int carPassengerLinks = countModeLinks(converter.getNetwork(), "car_passenger");
-		
-		// Since some car links are not connected, we expect that there are more (uncleaned) passenger links now
 		Assertions.assertEquals(carLinks + 8, carPassengerLinks);
 		
 		// II) Convert with a network layer for car_passenger
-		osmConfig.addParameterSet(new OsmConverterConfigGroup.RoutableSubnetworkParams("car_passenger", Collections.singleton("car")));
+		osmConfig.addParameterSet(new OsmConverterConfigGroup.RoutableSubnetworkParams("car_passenger", Set.of("car")));
 		
 		OsmMultimodalNetworkConverter converter2 = new OsmMultimodalNetworkConverter(osm);
 		converter2.convert(osmConfig);
-		
-		int carLinks2 = countModeLinks(converter2.getNetwork(), "car");
-		int carPassengerLinks2 = countModeLinks(converter2.getNetwork(), "car_passenger");
-		
+		Network network2 = converter2.getNetwork();
+		// NetworkUtils.writeNetwork(network2, "waterloo2.xml");
+
+		NetworkUtils.cleanNetwork(network2, Set.of("car"));
+
+		int carLinks2 = countModeLinks(network2, "car");
+		int carPassengerLinks2 = countModeLinks(network2, "car_passenger");
+
+		System.out.println("carLinks: " + carLinks);
+		System.out.println("carPassengerLinks: " + carPassengerLinks);
+		System.out.println("carLinks2: " + carLinks2);
+		System.out.println("carPassengerLinks2: " + carPassengerLinks2);
+
 		// Now car_passenger should be cleaned just as car... Thus it should be the same number.
 		Assertions.assertEquals(carLinks2, carPassengerLinks2);
 	}
